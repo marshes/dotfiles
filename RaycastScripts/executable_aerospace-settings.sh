8ace2bdcff565452b4735055c912450959d5046a
Matt <marshes@gmail.com>
2025-11-10 09:35:17 -0800

    Add RaycastScripts/aerospace-settings.sh

---
+++ RaycastScripts/executable_aerospace-settings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Aerospace Settings
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Open in Sublime
+
+subl ~/.config/aerospace/aerospace.toml
+echo "Opening Aerospace Settings"
