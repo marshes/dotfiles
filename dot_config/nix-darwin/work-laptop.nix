33397cf3a5ba08fbf63b1efc6282b7d8a5af135d
Matt <marshes@gmail.com>
2025-12-05 15:29:09 -0800

    Update .config/nix-darwin/work-laptop.nix

--- dot_config/nix-darwin/work-laptop.nix
+++ dot_config/nix-darwin/work-laptop.nix
@@ -5,11 +5,28 @@
   
   networking.hostName = "work-laptop";
   
-  homebrew.brews = [
-    "node"
-  ];
-  homebrew.cask =[
-  	"qlmarkdown"
-  ]
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+      cleanup = "zap"; # <-- uncommment when setting up a new system, deletes anything installed manually, i.e. not declared in this file
+    };
 
+    brews = [
+      "lazygit"
+      "node"
+      "uv"
+    ];
+
+    casks 
+    = [
+     "claude-code"
+     "qlmarkdown"
+    ];
+
+    taps = [
+    #"acsandmann/tap" #placeholder
+    ];
+  };
 }
